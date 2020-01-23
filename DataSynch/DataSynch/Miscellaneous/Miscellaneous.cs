6a3eb6db5589bccae868eaea539ce3b21fece905
DCW <DCW@DESKTOP-E78FF40>
2020-01-23 16:19:48 +0200

    Added Miscellaneous And SysTray classes

---
+++ DataSynch/DataSynch/Miscellaneous/Miscellaneous.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+namespace DataSynch.Miscellaneous
+{
+    /// <summary>
+    /// this class contains all special functions that have no place anywhere else
+    /// </summary>
+    class Miscellaneous
+    {
+        /// <summary>
+        /// the main function for closing the program
+        /// </summary>
+        public void ProgramClose()
+        {
+            Process.GetCurrentProcess().Kill();
+        }
+    }
+}
